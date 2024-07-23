1380d8b097717e58196492eeed5f1330f67fae41
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2024-07-23 21:47:33 +0300

    token handlers

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -14,6 +14,7 @@ export const useSignIn = () => {
                }
             }
          })
+         console.log(response)
          return response;
       } catch (error) {
          console.error(error);
